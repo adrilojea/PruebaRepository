165648ffa9f95a37e09dcf8dee16092c55936856
adrilojea <77686089+adrilojea@users.noreply.github.com>
2021-01-19 17:57:37 +0100

    PrimeraSubida
    Primera subida de la aplicacion.

---
+++ ibmtest/src/main/java/com/adri/ibmtest/IbmtestApplication.java
@@ -0,0 +1,13 @@
+package com.adri.ibmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmtestApplication.class, args);
+	}
+
+}
---
+++ ibmtest/src/test/java/com/adri/ibmtest/IbmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adri.ibmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
